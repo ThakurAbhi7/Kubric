e5fe761857beaddcb2f542f4e5555f630bdc8a41
Abhishek Thakur <ThakurAbhi7@users.noreply.github.com>
2020-11-18 23:32:13 +0530

    ploynomial regression

--- regression.py
+++ regression.py
@@ -3,6 +3,7 @@ import pandas
 import scipy
 import numpy
 import sys
+import matplotlib.pyplot as plt
 
 TRAIN_DATA_URL = "https://storage.googleapis.com/kubric-hiring/linreg_train.csv"
 TEST_DATA_URL = "https://storage.googleapis.com/kubric-hiring/linreg_test.csv"
@@ -19,21 +20,27 @@ def predict_price(area) -> float:
     ...
     import os, csv
 
-    os.system("wget "+TRAIN_DATA_URL)
-    os.system("wget "+TEST_DATA_URL)
+    # os.system("wget "+TRAIN_DATA_URL)
+    # os.system("wget "+TEST_DATA_URL)
     with open("linreg_train.csv", 'r') as csvfile:
         reader = csv.reader(csvfile)
         train_X = next(reader)[1:]
         train_Y = next(reader)[1:]
+    for i in range(len(train_X)):
+        train_X[i] = float(train_X[i])**0.5
     train_X = [train_X, [1]*len(train_X)]
     train_X = numpy.array(train_X).astype('float64').T
     train_Y = numpy.array(train_Y).astype('float64').T
-    I = numpy.array([[1, 0], [0, 1]])
-    XtX = numpy.linalg.inv(train_X.T.dot(train_X))  + I*140400550000
+    I = numpy.array([[0.8, 0], [0, 0.00008]])
+    XtX = numpy.linalg.inv(train_X.T.dot(train_X))  +10000000*I
     XtY = train_X.T.dot(train_Y)
     w, b = numpy.linalg.solve(XtX, XtY)
-    # print(w, b)
+    for i in range(len(area)):
+        area[i] = float(area[i])**0.5
     area = numpy.array(area).astype('float').T
+    plt.plot(train_X[:,0], train_Y, 'bs')
+    plt.plot([i for i in range(200)], [i*w+b for i in range(200)])
+    plt.show()
     return area*w+b
 
 if __name__ == "__main__":
